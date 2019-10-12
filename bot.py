7b35a57bebccecbdd2f22e5ce40c5a4e29051657
Taewan-P <swimingtw@gmail.com>
2019-10-12 22:45:20 +0900

    Initial Commit for bot.py
    New feature: When new personnel joins the server, he/her will get a greeting from the bot.

---
+++ bot.py
@@ -0,0 +1,45 @@
+import os, json
+import discord
+
+app = discord.Client()
+
+json_data = open(os.getcwd() + "/.discord_config").read()
+config_json = json.loads(json_data)
+
+token = config_json["token"]
+text_channel_general = config_json["text_channel_general"]
+footer = config_json["footer"]
+
+@app.event
+async def on_ready():
+    print('Logged in as')
+    print(app.user.name)
+    print(app.user.id)
+    print('------')
+    game = discord.Game("Jaram bot | $help")
+    await app.change_presence(status=discord.Status.online, activity=game)
+
+@app.event
+async def on_message(message):
+    if message.author.bot:
+        return None
+
+    if message.content == "$help":
+        embed = discord.Embed(title="명령어 목록", description="$help - 봇 도움말 불러오기 \n\n다른기능은 추후 추가예정.", color=0x6FA8DC)
+        embed.set_footer(text=footer)
+
+        embed2 = discord.Embed(title="To contribute", description="기능을 추가하려면 클릭하세요", url="https://github.com/Taewan-P/jaram-gaming-welcomebot/", color=0x6FA8DC)
+        embed2.set_footer(text=footer)
+        await message.channel.send(embed=embed)
+        await message.channel.send(embed=embed2)
+
+@app.event
+async def on_member_join(member):
+    channel = member.guild.get_channel(text_channel_general)
+
+    embed3 = discord.Embed(title="Welcome to Jaram Gaming Server!!", description=f"{member.mention}님. 누군지 알 수 있게 간단히 자기소개 부탁드려요~", color=0x6FA8DC)
+    embed3.set_footer(text=footer)
+
+    await channel.send(embed=embed3)
+
+app.run(token)
